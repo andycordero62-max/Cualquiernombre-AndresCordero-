c5bc8a2d60640acbb21dcc071defcfe71ba5f096
andycordero62-max <andycordero62@gmail.com>
2026-05-29 22:50:59 -0400

    El primer commit de este proyecto

---
+++ skibidisigmaponmi/skibidisigmaponmi/src/main/java/com/wintux/skibidisigmaponmi/SkibidisigmaponmiApplication.java
@@ -0,0 +1,13 @@
+package com.wintux.skibidisigmaponmi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkibidisigmaponmiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkibidisigmaponmiApplication.class, args);
+	}
+
+}
---
+++ skibidisigmaponmi/skibidisigmaponmi/src/test/java/com/wintux/skibidisigmaponmi/SkibidisigmaponmiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wintux.skibidisigmaponmi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkibidisigmaponmiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
